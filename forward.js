bbfba70343e7450b70a98859b29e363539fc4547
MsLolita <oblikovuizapuc@gmail.com>
2021-01-26 22:55:22 +0200

    change constructions of files from functions into classes; add infinite scroll(ajax), use-agents; sort content on photo and video; add second site from what parse(instagram), show used time on scraping, optimizationnnnn

--- forward.js
+++ forward.js
@@ -1,46 +1,100 @@
 'use strict'
 
 import puppeteer from "puppeteer";
+import userAgent from "user-agents";
 
-export async function scrapeUrl() {
-    const browser = await puppeteer.launch({
-        headless: false,
-        devtools: false,
-        // args: ["--start-maximized"]
-    });
-    let page =  await browser.newPage();
-    const imgSelector = "#container > .page .main-wrap > #list-view-2 div.list-stream article .post-container img";
-    const pageURL = 'https://9gag.com/funny/fresh';
+export default class ScrapeMedia {
+    #start = Date.now();
+    #browser;
+    #page;
 
-    try {
-        await page.goto(pageURL);
-
-    } catch (error) {
-        console.log(`I can't open: ${pageURL} due to this mistake: ${error}`);
+    constructor(site, typeMedia = "photo")  {
+        this.site = site;
+        this.typeMedia = typeMedia;
     }
 
-    await page.waitForSelector(imgSelector, { timeout: 0 });
-    //await page.screenshot({path: 'google.png'});
-    const pictureUrl = await page.$eval(
-        imgSelector, imgSelector => imgSelector.src
-    );
+    async startSession() {
+        this.#browser = await puppeteer.launch({
+            headless: false,
+            devtools: false,
+            // args: ["--start-maximized"]
+        });
 
-    // const resultedUrl = await page.evaluate(() => {
-    //     let url = document.querySelector("div.post-container picture img", { waitUntil: 'domcontentloaded' }).src;
-    //     return url;
-    // });
+        this.#page = await this.#browser.newPage();
 
-    //await page.reload({ waitUntil: ["networkidle0", "domcontentloaded"] })
+        await this.#page.setUserAgent(userAgent.toString());
+    }
 
-    browser.close();
-    return pictureUrl;
-};
+    async getMedia(_urlMedia) {
+        let mediaSelector = `.post-container ${this.typeMedia === 'photo' ? 'img' : 'video source'}`;
 
-// export function hello() {
-//     return "Hello";
-// }
+        await this.startSession();
+        await this.#page.goto(this.site);
+        try {
 
-//module.exports = { forwarBot };
-// let app = 123;
-//
-// module.exports = { app };
+            for(; !_urlMedia; await this.#page.evaluate('document.querySelector("#list-view-2 > div.loading").scrollIntoView()')){
+                await Promise.all(await this.#page.$$(`${mediaSelector.slice(0, 15)}:nth-child(n+2)`));
+                _urlMedia = await this.#page.evaluate( (mediaSelector) => {
+                    const post = document.querySelector(mediaSelector);
+                    return post && post.src;
+                }, mediaSelector);
+            }
+
+        } catch (error){
+            console.log("I can't find a picture due to " + error);
+        }
+        await this.endSession();
+
+        return _urlMedia;
+    }
+
+    async endSession() {
+        await this.#browser.close();
+
+        console.log('Used time:', (Date.now() - this.#start) / 1000);
+    }
+
+    async getMediaInsta(_urlMedia) {
+        this.typeMedia = this.typeMedia === 'photo' ? 'display_url' : 'video_url';
+
+        await this.startSession();
+        await this.#page.goto(this.site + '?__a=1');
+
+        try {
+
+            _urlMedia = this.findMediaInsta(
+                await this.#page.evaluate( () => {
+                    return JSON.parse(document.body.getElementsByTagName('pre')[0].innerText);
+                })
+            )[0];
+
+        } catch (e) {
+            console.log("I can't find a picture due to " + e);
+        }
+
+        await this.endSession();
+        return _urlMedia;
+    }
+
+    async defineSite(site) {
+        const hostname = new URL(site).hostname;
+        let _urlMedia = '';
+        switch (hostname) {
+            case '9gag.com':
+                return await this.getMedia(_urlMedia);
+            case 'www.instagram.com':
+                return await this.getMediaInsta(_urlMedia);
+            default:
+                return 'I don\'t know how to scrape this site';
+        }
+    }
+
+    findMediaInsta(obj, res = []) {
+        for (const key in obj)
+            if (typeof obj[key] !== 'string')
+                this.findMediaInsta(obj[key], res);
+            else if (key === this.typeMedia)
+                res.push(obj[key]);
+        return res;
+    }
+}
